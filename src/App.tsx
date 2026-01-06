f1370f5b0cf2d4c2d0ffbd9481455717b4d6882a
Vu Phuc Quang Vinh <vinhvpq2004@gmail.com>
2026-01-06 14:13:22 +0700

    feat: implement main layout and evaluation component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import MainLayout from "./layouts/MainLayout";
 import Evaluation from "./pages/Evaluation";
-import React from "react";
 export default function App() {
   return (
-    <></>
+    <>
+      <MainLayout>
+        <Evaluation />
+      </MainLayout>
+    </>
   );
 }
